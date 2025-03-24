41a8787cb2f779e7e7e74a2383a5848727a578ef
Shreyas Gowda <shreyasgowda1901@gmail.com>
2025-03-24 22:51:35 +0530

    Fix GlobeVisualization import path in Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import { ThemeContext } from '../context/ThemeContext';
-import GlobeVisualization from '../components/globe/GlobeVisualization';
+import GlobeVisualization from '../components/GlobeVisualization';
 import DonationStats from '../components/DonationStats';
 import AwarenessQuotes from '../components/AwarenessQuotes';
 
